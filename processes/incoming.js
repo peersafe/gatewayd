418165b1a515ca88ed4f1c825f7fc0ac7baf933e
chengshuangquan <chengshuangquan@peersafe.cn>
2016-02-03 16:27:25 +0800

    同上备注

--- processes/incoming.js
+++ processes/incoming.js
@@ -2,8 +2,11 @@ var Promise                 = require('bluebird');
 var gatewayd                = require(__dirname+'/../');
 var RippleAccountMonitor    = require('ripple-account-monitor');
 var IncomingPayment         = require(__dirname+'/../lib/core/incoming_payment.js');
-const coldWallet            = gatewayd.config.get('COLD_WALLET');
-const rippleRestBaseUrl     = gatewayd.config.get('RIPPLE_REST_API');
+const coldWallet            = gatewayd.config.get('COLD_WALLET');  //const
+const rippleRestBaseUrl     = gatewayd.config.get('RIPPLE_REST_API'); //const
+var exec = require('child_process').exec; 
+var RippleAPI = require('ripple-lib').RippleAPI; //const
+var hash =  require('../payment-hash.js');
 
 function Monitor(gatewayd) {
   return new RippleAccountMonitor({
@@ -21,29 +24,51 @@ function Monitor(gatewayd) {
         });
     },
     onPayment: function(paymentNotification, next) {
+debugger;
       var incomingPayment = new IncomingPayment(paymentNotification);
       incomingPayment.processPayment()
         .then(function(processedPayment){
-          gatewayd.logger.info('payment:incoming:recorded', JSON.stringify(processedPayment));
+          //gatewayd.logger.info('payment:incoming:recorded', JSON.stringify(processedPayment));
           next();
         })
         .error(function(error){
-          gatewayd.logger.error('payment:incoming:error', error);
+          //gatewayd.logger.error('payment:incoming:error', error);
           next();
         });
     },
     onError: function(error) {
-      gatewayd.logger.error('RippleAccountMonitor::Error', error);
+     debugger;
+    	if(hash._d.v) //add code by csquan
+	{
+		console.log("################################################");
+		console.log("want set to hash:" + hash._d.v);
+
+		cmdStr = '/home/shuangquan/work/gatewayd/bin/gateway set_last_payment_hash '+ hash._d.v;
+		console.log(cmdStr);
+		exec(cmdStr, function(err,stdout,stderr){
+    			if(err) {
+        			console.log('***************set_last_payment_hash error:***********');
+        			console.log(stderr);
+    			} 
+    			else {
+				console.log("*********set_last_payment_hash success*****************");
+                        	process.exit(0);
+    			}
+		});
+	}
     }
   });
 }
 
 function start(gatewayd) {
-  var monitor = new Monitor(gatewayd);
+var monitor = new Monitor(gatewayd);
+debugger;
   gatewayd.api.getOrFetchLastPaymentHash()
     .then(function(paymentHash){
       monitor.lastHash = paymentHash;
       monitor.start();
+gatewayd.logger.info("conenect at ",rippleRestBaseUrl);
+gatewayd.logger.info("coldWallet : ",coldWallet);
       gatewayd.logger.info('Listening for incoming ripple payments from Ripple REST, starting at', monitor.lastHash);
     });
 }
